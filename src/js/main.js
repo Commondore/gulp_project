f0b897751ad6f3e49f2a1b3346bccaaac4cd21f5
Alex Som <alex.som86@gmail.com>
2021-09-03 09:51:41 +0600

    add slider base

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+import Swiper, { Navigation, Pagination } from "swiper";
+
+Swiper.use([Navigation, Pagination]);
+
+new Swiper("#main-slide", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
